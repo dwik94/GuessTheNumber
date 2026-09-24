81beda729185347a93b1943f247e23570e915e61
Dennis <dennis.wiklund@telia.com>
2026-09-24 21:58:52 +0200

    Fixed connection to console

--- src/main/java/com/dennis/Main.java
+++ src/main/java/com/dennis/Main.java
@@ -1,12 +1,17 @@
 package com.dennis;
 import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     static void main() {
 
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         int secretNumber = 1 + random.nextInt(100);
+        int guessedInt = scanner.nextInt();
+        IO.println(guessedInt);
 
-        IO.println(secretNumber);
+        scanner.close();
     }
 }
